ba572413c81e92a34f1e667aec7165d0e8e2ad92
Sarah Matsuura <matsuus1@uci.edu>
2025-03-11 21:18:22 +0000

    added load_words and print_word_ladder, made gtests for these and generate_word_ladder

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -3,12 +3,13 @@
 #include "dijkstras.h"
 #include "ladder.h"
 
-TEST(LadderTests, EditDistanceWithin) {                       // distance
-  EXPECT_TRUE(edit_distance_within("sitting", "kitten", 4));  // 3
-  EXPECT_TRUE(edit_distance_within("Sunday", "Saturday", 4)); // 3
-  EXPECT_TRUE(edit_distance_within("mafuyu", "mizuki", 5));   // 4
-  EXPECT_FALSE(edit_distance_within("kanade", "canada", 2));  // 2
-  EXPECT_FALSE(edit_distance_within("ena", "enanan", 3));     // 3
+TEST(LadderTests, EditDistanceWithin) {
+  EXPECT_TRUE(edit_distance_within("sitting", "kitten", 3));
+  EXPECT_TRUE(edit_distance_within("Sunday", "Saturday", 3));
+  EXPECT_TRUE(edit_distance_within("mafuyu", "mizuki", 4));
+  EXPECT_TRUE(edit_distance_within("kanade", "canada", 2));
+  EXPECT_TRUE(edit_distance_within("ena", "enanan", 3));
+  EXPECT_FALSE(edit_distance_within("mizuki", "amia", 1));
 }
 
 TEST(LadderTests, IsAdjacent) {
@@ -17,9 +18,30 @@ TEST(LadderTests, IsAdjacent) {
   EXPECT_TRUE(is_adjacent("car", "cat"));
   EXPECT_TRUE(is_adjacent("cat", "chat"));
   EXPECT_TRUE(is_adjacent("chat", "cheat"));
+  EXPECT_FALSE(is_adjacent("kanade", "KaNaDe"));
   EXPECT_FALSE(is_adjacent("mafuyu", "yuki"));
 }
 
-TEST(LadderTests, GenerateWordLadder) {
-  // ah
+TEST(LadderTests, LoadWords) {
+  set<string> s;
+  load_words(s, "./src/words.txt");
+  EXPECT_NE(s.find("Aarhus"), s.end());
+  EXPECT_NE(s.find("leaf"), s.end());
+  EXPECT_NE(s.find("Zurich"), s.end());
+  EXPECT_EQ(s.find("asjfklsdh"), s.end());
 }
+
+TEST(LadderTests, GenerateWordLadder) {
+  set<string> s;
+  load_words(s, "./src/words.txt");
+  EXPECT_EQ(generate_word_ladder("piss", "poop", s), vector<string>({}));
+  EXPECT_EQ(generate_word_ladder("wonderhoy", "wonderhoy", s), vector<string>({}));
+  EXPECT_EQ(generate_word_ladder("peek", "peeks", s), vector<string>({"peek", "peeks"}));
+}
+
+TEST(LadderTests, PrintWordLadder) {
+  set<string> s;
+  load_words(s, "./src/words.txt");
+  print_word_ladder(generate_word_ladder("cat", "dog", s));
+}
+
--- src/ladder.cpp
+++ src/ladder.cpp
@@ -8,7 +8,15 @@
 // words.txt is the dictionary, ignore letter cases
 
 void error(string word1, string word2, string msg) {
-    cout << "(" << word1 << ", " << word2 << "): " << msg << endl;
+    cerr << "(" << word1 << ", " << word2 << "): " << msg << endl;
+}
+
+void print_table(int * dist_table, int i, int j) { // helper function for testing
+    for (int n=0; n <= i; ++n) {
+        for (int m=0; m <= j; ++m)
+            cout << dist_table[m + n*i] << ' ';
+        cout << endl;
+    }
 }
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
@@ -20,55 +28,63 @@ bool edit_distance_within(const std::string& str1, const std::string& str2, int 
     int substitution_cost;
     for (int j=1; j <= str2.length(); ++j) {
         for (int i=1; i <= str1.length(); ++i) {
-            if (str1[i-1] == str2[j-1]) { substitution_cost = 0; }
+            if (tolower(str1[i-1]) == tolower(str2[j-1])) { substitution_cost = 0; }    // ignore letter case
             else { substitution_cost = 1; }
             dist_table[i][j] = min(min(dist_table[i-1][j] + 1,        // insertion
                           dist_table[i][j-1] + 1),                    // deletion
                           dist_table[i-1][j-1] + substitution_cost);  // substitution
         }
     }
-    // for (int i=0; i <= str1.length(); ++i) {
-    //     for (int j=0; j <= str2.length(); ++j)
-    //         cout << dist_table[i][j] << ' ';
-    //     cout << endl;
-    // }
-    return dist_table[str1.length()][str2.length()] < d;  // d is the threshold for difference between 2 words
+    // print_table((int*)dist_table, str1.length(), str2.length());
+    return dist_table[str1.length()][str2.length()] == d;  // d is the threshold for difference between 2 words
 }
 
 bool is_adjacent(const string& word1, const string& word2) {
     int diff = word1.length() - word2.length();
     if (diff < -1 || 1 < diff) { return false; }
-    return edit_distance_within(word1, word2, 2);
+    return edit_distance_within(word1, word2, 1);
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
-    queue<vector<string>> ladder_queue;
-    ladder_queue.push({begin_word});
-    set<string> visited;
-    while (!ladder_queue.empty()) {
-        vector<string> ladder = ladder_queue.front();
-        ladder_queue.pop();
-        string last_word = ladder.back();
-        for (auto & word : word_list) {
-            if (is_adjacent(last_word, word) && visited.find(word) != visited.end()) {
-                visited.insert(word);
-                vector<string> new_ladder = ladder;
-                new_ladder.push_back(word);
-                if (word == end_word) {
-                    return new_ladder;
-                } ladder_queue.push(new_ladder);
+    if (begin_word == end_word) { error(begin_word, end_word, "start and end words cannot be the same"); }
+    else if (word_list.find(end_word) == word_list.end()) { error(begin_word, end_word, "invalid end word"); }
+    else {
+        queue<vector<string>> ladder_queue;
+        ladder_queue.push({begin_word});
+        set<string> visited;
+        while (!ladder_queue.empty()) {
+            vector<string> ladder = ladder_queue.front();
+            ladder_queue.pop();
+            string last_word = ladder.back();
+            for (auto & word : word_list) {
+                if (is_adjacent(last_word, word) && (visited.find(word) == visited.end())) {
+                    visited.insert(word);
+                    vector<string> new_ladder = ladder;
+                    new_ladder.push_back(word);
+                    if (word == end_word) {
+                        return new_ladder;
+                    } ladder_queue.push(new_ladder);
+                }
             }
         }
     } return vector<string>();
 }
 
 void load_words(set<string> & word_list, const string& file_name) {
-    // kms
+    ifstream file(file_name);
+    for (string word; file >> word;) {
+        word_list.insert(word);
+    } file.close();
 }
 
 void print_word_ladder(const vector<string>& ladder) {
-    // kms
+    for (auto word : ladder) {
+        cout << word << ' ';
+    }
 }
+
+#define my_assert(e) {cout << #e << ((e) ? " passed" : " failed") << endl;}
 void verify_word_ladder() {
-    // kms
+    set<string> word_list;
+    // ...
 }
